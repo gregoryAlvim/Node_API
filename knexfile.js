cb411f9518ea0894a20f338f29fef483a23b3dd9
Grégory Alvim <gregori.jagua@gmail.com>
2022-08-27 22:06:07 -0300

    Add config foreing Key

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,10 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")    
     },
+
+    pool: {
+      afterCreate: (connection, callback) => connection.run("PRAGMA foreign_keys = ON", callback)
+    },
     
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
