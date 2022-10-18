779add6384dad5ec1badb54ad380cff9546442f6
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-17 23:17:53 -0300

    Feat: added Sessions routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const routes = Router();
 const usersRoutes = require("./users.routes");
 const movieNotesRoutes = require("./movieNotes.routes");
 const movieTagsRoutes = require("./movieTags.routes");
+const sessionsRoutes = require("./sessions.routes");
 
 
 routes.use("/users", usersRoutes);
 routes.use("/movie-notes", movieNotesRoutes);
 routes.use("/movie-tags", movieTagsRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 module.exports = routes;
 
