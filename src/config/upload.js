40a7fba7a50b1a6befb88c6628284f0b1df4ab7f
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-19 14:24:17 -0300

    Reafactor: resolved problens and ajusted the storage by multer

--- src/config/upload.js
+++ src/config/upload.js
@@ -3,10 +3,10 @@ const multer = require("multer");
 const crypto = require("crypto");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER = {
-   storage: multer.localStorage({
+   storage: multer.diskStorage({
       destination: TMP_FOLDER,
       filename( request, file, callback ) {
          const fileHash = crypto.randomBytes(10).toString("hex");
