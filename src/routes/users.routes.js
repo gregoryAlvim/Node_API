4f15df22da6b512e8cbc7f3aef5956930adcc11e
Grégory Alvim <gregori.jagua@gmail.com>
2022-10-19 14:27:35 -0300

    Feat: added multer and router for user avatar upload

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,21 @@
 const { Router } = require("express");
-const UsersController = require("../controllers/UsersController");
+const multer = require("multer");
+
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const UsersController = require("../controllers/UsersController");
+const uploadConfig = require("../config/upload");
+
+const userAvatarController = new UserAvatarController();
 const usersController = new UsersController();
+const upload = multer(uploadConfig.MULTER);
 const usersRoutes = Router();
+
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.get("/show-user", ensureAuthenticated, usersController.show);
 usersRoutes.get("/", ensureAuthenticated, usersController.index);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
